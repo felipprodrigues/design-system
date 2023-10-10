b1b5ee227f0d7a8516534ab8714b4e209e320c25
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-10-10 14:56:34 -0300

    remove unused import

--- packages/react/src/components/MultiStep/index.tsx
+++ packages/react/src/components/MultiStep/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 
-import { MultiStep } from '../..'
 import { Label, MultiStepContainer, Step, Steps } from './styles'
 
 export interface MultiStepProps {
