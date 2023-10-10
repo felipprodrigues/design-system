fc4bf63a9c830878f89cd525711fb3f6a11d1253
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-10-10 16:43:54 -0300

    fix: add subpath to storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -26,5 +26,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, {configType}) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/design-system'
+    }
+
+    return config
+  }
 };
 export default config;
